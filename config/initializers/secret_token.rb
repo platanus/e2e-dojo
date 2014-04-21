a63b19b07d8bc05516cf12a3d7709e2c94d5d8d2
Leandro Segovia <ldlsegovia@gmail.com>
2014-04-21 18:05:32 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+E2eDojo::Application.config.secret_key_base = '1dbfcb5f71b72463a0f19330930aa1f3c88218c079c33d2257f5f63635192278ff3df527e1162a13d30c3adf1b6332ac60d55e3e910eabfd4c1b5b18308ea9cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+E2eDojo::Application.config.session_store :cookie_store, key: '_e2e-dojo_session'
